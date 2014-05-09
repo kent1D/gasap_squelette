efa1a59dc8cbbf5fec424099f4c347428abdca0e
kent1 <kent1@arscenic.info>
2014-05-09 15:47:28 +0200

    Début d'i18n correct
    Pas de <?xml version="1.0" encoding="#CHARSET"?> en entête de squelette
    
    Autres broutilles
    
    On en fait un plugin (avec paquet.xml)

---
+++ lang/gasap_skel_fr.php
@@ -0,0 +1,30 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'alt_bruxelles' => 'Bruxelles',
+	'alt_ibge' => 'IBGE',
+
+	// I
+	'info_derniere_maj' => 'Derni&egrave;re mise à jour&nbsp;:',
+	'info_soutien' => 'Avec le soutien de&nbsp;:',
+	
+	// L
+	'lire_plus' => 'lire plus',
+	
+	// P
+	'plus_actu' => 'lire plus d\'actualités',
+
+	// T
+	'titre_reseau' => 'le réseau bruxellois',
+	'titre_telecharger' => 'À télécharger',
+	'titre_petites_annonces' => 'Les petites annonces du Réseau',
+	'titre_trouver_gasap' => 'Trouvez un GASAP près de chez vous!'
+
+);
+
+?>
---
+++ lang/gasap_skel_nl.php
@@ -0,0 +1,29 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'alt_bruxelles' => 'Brussel',
+	'alt_ibge' => 'BIM',
+
+	// I
+	'info_derniere_maj' => 'Laatst aangepast:',
+	'info_soutien' => 'Met de steurn van:',
+
+	// L
+	'lire_plus' => 'lees meer',
+
+	// P
+	'plus_actu' => 'meer nieuws',
+
+	// T
+	'titre_reseau' => 'het brusselse netwerk',
+	'titre_telecharger' => 'Binnenhalen',
+	'titre_petites_annonces' => 'Kleine Mededelingen',
+	'titre_trouver_gasap' => 'Vind een SAGAL in uw buurt!'
+);
+
+?>
