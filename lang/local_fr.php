eb29332da342312ad2976b4f9f2df3ebc5cf199c
kent1 <kent1@arscenic.info>
2014-05-09 13:30:46 +0200

    La version originale

---
+++ lang/local_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'plus_actu' => 'lire plus d\'actualités',
+'lire_plus' => 'lire plus'
+
+
+);
+
+?>
